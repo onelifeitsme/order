259d190888fc08d1bd3354196bf181051c4a0526
Artur <onelifeitsme@gmail.com>
2022-03-11 02:25:45 +0300

    almost_done'

--- mysite/order/tests/conftest.py
+++ mysite/order/tests/conftest.py
@@ -7,9 +7,8 @@ def new_type(db) -> ProductType:
     """Фикстура создания типа продукта"""
     return ProductType.objects.create(name='Тип товара номер 1')
 
+
 @pytest.fixture
 def new_product(db, new_type) -> Product:
     """Фикстура создания товара"""
     return Product.objects.create(type=new_type, name='Название товара номер 1')
-
-
